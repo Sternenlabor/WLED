6fef1fe0202830e6881ba26037f08ee7a6b481c3
Holger Weber <holger@webers-web.net>
2024-02-27 23:52:07 +0100

    GUI working with default watchface.

--- usermods/usermod_v2_word_clock24/generator/wcgen.py
+++ usermods/usermod_v2_word_clock24/generator/wcgen.py
@@ -2,7 +2,46 @@ from nicegui import ui
 import json
 import pathlib
 
-arrButtons = []
+
+strFile = "wc_test_4x4.json"
+strFile = "wc_default.json"
+
+arrButtons = None
+print(arrButtons)
+
+
+
+class ToggleButton(ui.button):
+
+    def __init__(self, *args, **kwargs):
+        self._state = False
+        if "idx" in kwargs.keys():
+           self._idx = kwargs["idx"]
+           del kwargs["idx"]
+        super().__init__(*args, **kwargs)
+        #self.on('click', self.toggle)
+
+
+    def toggle(self) -> None:
+        """Toggle the button state."""
+        self._state = not self._state
+        self.update()
+
+    def setOff(self) -> None:
+        self._state = False
+        self.update()
+
+
+    def update(self):
+        #self._state = False
+        self.props(f'color={"black" if not self._state else "green"}')
+        super().update()
+
+class BindValues:
+    def __init__(self):
+        self.strSelectedIdx = ""
+
+bindValues = BindValues()
 
 def LoadWordClockJson(strFileName):
    dictWorcClockConfig = None
@@ -10,6 +49,14 @@ def LoadWordClockJson(strFileName):
        dictWorcClockConfig = json.load(f)
    return dictWorcClockConfig
    
+   
+def CheckWatchFace(arrWatchface):
+   width = len(arrWatchface[0])   
+   for row in arrWatchface:
+      if len(row) != width:
+         raise RuntimeError("Size mismatch in row:" + row)
+   
+         
 
 def CreateMeanderMap(arrWatchface):
    # create idx array
@@ -41,31 +88,43 @@ def CreateMeanderMap(arrWatchface):
    print("3. Step: ", arrFlat)
    return arrFlat
 
-def ToggleButton(idx, button: ui.button):
-   print(dir(button))
-   print("Style:", button.default_classes)
-   print("Props:", button.default_props)
-   pass
+def CbToggleButton(idx, button: ui.button):
+   button.toggle()
+   arr = []
+   for b in arrButtons:
+      if b._state:
+         arr.append(b._idx)
+   arr.sort()
+   bindValues.strSelectedIdx = str(arr)
 
+def CbClearAll():
+   for b in arrButtons:
+      b.setOff()
+   
 def main():
-   confiWc = LoadWordClockJson("wc_test_4x4.json")
+   global arrButtons
+   confiWc = LoadWordClockJson(strFile)
    arrOrig = confiWc["apperance"]["watchface"]
+   CheckWatchFace(arrOrig)
    arrMap = CreateMeanderMap(arrOrig)
 
-   
-   
+ 
    print("Create html")
    ## show watch face in web frontend
    with ui.grid(columns=len(arrOrig[0])):
       iIdx = 0
       for row in arrOrig:
          for letter in row:
-            arrButtons.extend(ui.button(letter, on_click=lambda e, idButton=arrMap[iIdx]: ToggleButton(idButton, e.sender)).tooltip(str(arrMap[iIdx])))
-            #arrButtons.extend(ui.button(letter, color="red", on_click=lambda idButton=arrMap[iIdx]: ToggleButton(idButton)).tooltip(str(arrMap[iIdx])))
+            btTmp = ToggleButton(letter, on_click=lambda e, idButton=arrMap[iIdx]: CbToggleButton(idButton, e.sender), idx = arrMap[iIdx]).tooltip(str(arrMap[iIdx]))
+            if arrButtons == None:
+               arrButtons = [btTmp]
+            else:
+               arrButtons.extend([btTmp])
             
             iIdx += 1
            
-   print(arrButtons)
+   ui.button("Clear", on_click=lambda: CbClearAll())
+   ui.input('Selected LEDs').bind_value(bindValues, "strSelectedIdx")
 
    ui.run()
 
